b0456b3dddc92b026582dbcecd8651c3522533ff
gd0951 <deolgurtaj0@gmail.com>
2026-05-08 10:53:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const elements = document.querySelectorAll('.fade');
+
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 100) {
+      el.classList.add('show');
+    }
+  });
+});
